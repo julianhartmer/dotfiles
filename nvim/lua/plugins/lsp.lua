913434a627f840da268542c6a57d93b378d38945
Julian Hartmer <j.hartmer@gmail.com>
2026-03-30 17:30:54 +0200

    Fix nvim errors, push to 0.12 version of nvim
    Signed-off-by: Julian Hartmer <j.hartmer@gmail.com>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,18 +2,14 @@
 
 return {
   {
-    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
     config = function()
-      local lspconfig = require("lspconfig")
       local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
       local on_attach = function(client, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
-        
+
         -- Set keybinds
         vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
@@ -27,8 +23,11 @@ return {
 
       local capabilities = cmp_nvim_lsp.default_capabilities()
 
-      -- Setup language servers
-      lspconfig["lua_ls"].setup({
+      -- Lua Language Server
+      vim.lsp.config("lua_ls", {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = { ".luarc.json", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", ".git" },
         capabilities = capabilities,
         on_attach = on_attach,
         settings = {
@@ -43,6 +42,45 @@ return {
           },
         },
       })
+
+      -- Python Language Server
+      vim.lsp.config("pyright", {
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+            },
+          },
+        },
+      })
+
+      -- TypeScript/JavaScript Language Server
+      vim.lsp.config("ts_ls", {
+        cmd = { "typescript-language-server", "--stdio" },
+        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+        root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- Bash Language Server
+      vim.lsp.config("bashls", {
+        cmd = { "bash-language-server", "start" },
+        filetypes = { "sh", "bash" },
+        root_markers = { ".git" },
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- Enable all language servers
+      vim.lsp.enable({ "lua_ls", "pyright", "ts_ls", "bashls" })
     end,
   },
   {
